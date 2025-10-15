f43e6cef8a1b36c70f4a5ad774652f2d91d99004
jairrmattos <jair.r.mattos@gmail.com>
2025-10-14 23:09:52 -0300

    Update settings.py

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'erp'
 ]
 
 MIDDLEWARE = [
@@ -119,4 +120,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
 
