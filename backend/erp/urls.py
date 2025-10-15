0f274a6a5acb83a36af0a8d34223237d17e738fe
jairrmattos <jair.r.mattos@gmail.com>
2025-10-14 23:16:27 -0300

    Update urls.py

--- backend/erp/urls.py
+++ backend/erp/urls.py
@@ -4,10 +4,3 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
 ]
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('erp.urls')),
-]
